c59865cbe776c5081ebe374c1449cf03c0b332fc
lipe <liipelovemoon@gmail.com>
2025-08-31 22:13:28 -0300

    feat: pular tentativa de saque para SavingsAccount

--- src/main/java/br/com/mariojp/solid/lspaccounts/BankService.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/BankService.java
@@ -2,6 +2,10 @@ package br.com.mariojp.solid.lspaccounts;
 
 public class BankService {
     public void processWithdrawal(Account acc, double amount){
-        acc.withdraw(amount);
+    	if (acc instanceof SavingsAccount){
+    		System.out.println("Esse tipo de conta não possui permissão para saque.");
+    	} else {
+    		acc.withdraw(amount);
+    	}
     }
 }
